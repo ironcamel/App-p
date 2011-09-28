01431efdacf470b537b4a69ad721e0d65bcff690
Naveed Massjouni <naveedm9@gmail.com>
2011-09-28 19:10:06 -0400

    Fixed typos in docs

--- lib/App/p.pm
+++ lib/App/p.pm
@@ -10,7 +10,7 @@ package App::p;
 
     p 'dd [File::Spec->path]'   # dynamically load arbitrary modules
     p -pe 's/foo/bar/' foo.txt  # use your favorite options like -lane
-    p 'say "hello world!"       # -E is assumed if no options are provided
+    p 'say "hello world!"'      # -E is assumed if no options are provided
     p 'dd yl r "config.yml"'    # chain commands
     p 'dd ExtUtils::Installed->new->modules' # list all installed modules
 
